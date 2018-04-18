5ba1d8cc529c618331d0ef69ea4ada7251f780db
garciaalberto <Bertooooo10@gmail.com>
2018-04-18 21:10:38 +0200

    Theorical database created

--- SharedDoorsAPP/models.py
+++ SharedDoorsAPP/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Flat(models.Model):
+    name = models.CharField(max_length=20)
+    key = models.CharField(max_length=20)
+
+class User(models.Model):
+    name = models.CharField(max_length=25)
+    password = models.CharField(max_length=20)
+    mail = models.CharField(max_length=25)
+    points_total = models.IntegerField()
+    points_mensual = models.IntegerField()
+    allocation = models.ForeignKey(Flat, null=True, on_delete=models.SET_NULL)
+
+class Event(models.Model):
+    day = models.DateField()
+    name = models.CharField(max_length=20)
+    price = models.IntegerField(null=True)
+    allocation = models.ForeignKey(Flat, on_delete=models.CASCADE)
+
+class EventUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Event, on_delete=models.CASCADE)
+

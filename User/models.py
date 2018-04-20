a9fe90bc896098206085b56393ae8786e46aa0d3
garciaalberto <Bertooooo10@gmail.com>
2018-04-20 19:17:15 +0200

    User table created successfully

--- User/models.py
+++ User/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=25)
+    password = models.CharField(max_length=20)
+    mail = models.CharField(max_length=25)
+    points_total = models.IntegerField()
+    points_monthly = models.IntegerField()
+    #allocation = models.ForeignKey(Flat, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name + " " + self.id;

a313e4921783bb011e928adc08dd2931534ebebe
garciaalberto <Bertooooo10@gmail.com>
2018-04-20 17:43:57 +0200

    Connection working

--- SharedDoorsAPP/models.py
+++ SharedDoorsAPP/models.py
@@ -13,13 +13,20 @@ class User(models.Model):
     points_mensual = models.IntegerField()
     allocation = models.ForeignKey(Flat, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.name
+
 class Event(models.Model):
     day = models.DateField()
     name = models.CharField(max_length=20)
     price = models.IntegerField(null=True)
     allocation = models.ForeignKey(Flat, on_delete=models.CASCADE)
+    type = (
+        ('M', 'Menu'),
+        ('T', 'Task'),
+        ('P', 'Payment'),
+    )
+    users = models.ManyToManyField(User)
 
-class EventUser(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    task = models.ForeignKey(Event, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.name

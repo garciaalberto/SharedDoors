c71e7a69fb7dda03e01f5a1573807a6a01b8c79b
garciaalberto <Bertooooo10@gmail.com>
2018-04-22 11:10:19 +0200

    Register and login templates createed

--- APP/views.py
+++ APP/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
+user = None
+flat = None
+
 
 def index(request):
     context = {
-        'title': 'Identification'
+        'title': 'Welcome to Shared Doors'
     }
     return render(request, '../SharedDoors-templates/APP/index.html', context)
+
+
+def login(request):
+    context = {
+        'title': 'Log In'
+    }
+    return render(request, '../SharedDoors-templates/APP/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'Register'
+    }
+    return render(request, '../SharedDoors-templates/APP/register.html', context)
--- SharedDoors/urls.py
+++ SharedDoors/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^dev/', admin.site.urls),
-    url(r'^$', include('APP.urls')),
+    url(r'^app/', include('APP.urls')),
 ]
